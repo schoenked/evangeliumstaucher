d070fcee9af3353dc6ed0758c2c6da893d5a5b67
Daniel <schoenke.dan@gmail.com>
2024-02-17 17:31:32 +0100

    intelligetn start stop script

--- app/gospeldiver.sh
+++ app/gospeldiver.sh
@@ -2,17 +2,32 @@
 
 APP_JAR=app.jar
 
-if [ "$1" = "start" ]; then
+start_app() {
+  stop_app
   java -jar "$APP_JAR" &
-  # Get the PID of the last background job
   background_pid=$!
+  echo $background_pid > gospeldiver.pid
+}
 
-  # Write the PID to a file
-  echo $background_pid > app.pid
+stop_app() {
+  if [ -f gospeldiver.pid ]; then
+    pid=$(cat gospeldiver.pid)
 
+    if ps -p $pid > /dev/null; then
+      kill -15 $pid
+      rm gospeldiver.pid
+    else
+      echo "PID file exists but the process is not running."
+    fi
+  else
+    echo "PID file does not exist. The application may not be running."
+  fi
+}
+
+if [ "$1" = "start" ]; then
+  start_app
 elif [ "$1" = "stop" ]; then
-  echo "no support"
-  #java -cp "$APP_JAR:<other jars>" $APP_MAIN stop
+  stop_app
 else
   echo "Usage: $0 start|stop"
 fi
