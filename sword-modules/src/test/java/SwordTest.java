cf54a92bfd528ef545fadeb51310d2a58b4fec76
Daniel <schoenke.dan@gmail.com>
2024-08-17 21:30:39 +0200

    unit test

---
+++ sword-modules/src/test/java/SwordTest.java
@@ -0,0 +1,46 @@
+import org.crosswire.jsword.book.*;
+import org.crosswire.jsword.book.install.InstallException;
+import org.crosswire.jsword.book.install.InstallManager;
+import org.crosswire.jsword.book.install.Installer;
+import org.crosswire.jsword.book.sword.SwordBook;
+import org.crosswire.jsword.passage.Key;
+import org.crosswire.jsword.passage.NoSuchKeyException;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class SwordTest {
+
+    @Test
+    public void testLoaqdModule() throws NoSuchKeyException, BookException, InstallException, InterruptedException {
+        InstallManager imanager = new InstallManager();
+        Installer i = imanager.getInstaller("CrossWire");
+        List<SwordBook> books = i.getBooks(BookFilters.getBibles())
+                .stream().filter(b -> b instanceof SwordBook)
+                .map(book -> (SwordBook) book)
+                .filter(swordBook -> {
+                    return swordBook.getBookMetaData().getName().equals("Neue evangelistische Übersetzung");
+                })
+                .toList();
+        SwordBook book = books.get(0);
+        i.install(book);
+        book = (SwordBook) Books.installed().getBook(book.getName());
+      /*  for (int j = 0; j < 1000000; j++) {
+            Verse verse = new Verse(book.getVersification(),
+                    j);
+            BookData bookData = new BookData(book, verse);
+            String canonicalText = OSISUtil.getCanonicalText(bookData.getOsisFragment());
+            String rawText = book.getRawText(verse);
+            System.out.println(verse.getName() + ":"+ canonicalText);
+            //Thread.sleep(1);
+        }*/
+        Key key = book.getKey("Joh 3:16");
+        BookData bookData = new BookData(book, key);
+
+        String canonicalText = OSISUtil.getCanonicalText(bookData.getOsisFragment());
+        assertThat(canonicalText).contains("so hat Gott der Welt seine Liebe gezeigt");
+    }
+
+}
