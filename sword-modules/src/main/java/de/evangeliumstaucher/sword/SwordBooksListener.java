a53fc6675546370caa58e9d92fa9565feb0e548d
Daniel <schoenke.dan@gmail.com>
2024-08-29 22:45:20 +0200

    installer waiting fix

--- sword-modules/src/main/java/de/evangeliumstaucher/sword/SwordBooksListener.java
+++ sword-modules/src/main/java/de/evangeliumstaucher/sword/SwordBooksListener.java
@@ -1,25 +1,27 @@
 package de.evangeliumstaucher.sword;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.crosswire.jsword.book.Book;
 import org.crosswire.jsword.book.BooksEvent;
 import org.crosswire.jsword.book.BooksListener;
 
 import java.util.concurrent.Semaphore;
 
+@Getter
+@Slf4j
 public class SwordBooksListener implements BooksListener {
     private final Semaphore lock;
     Book installedBook;
 
-    public SwordBooksListener(Semaphore lock) {
-        this.lock = lock;
-    }
-
-    public Book getInstalledBook() {
-        return installedBook;
+    public SwordBooksListener() {
+        this.lock = new Semaphore(1);
+        this.lock.acquireUninterruptibly();
     }
 
     @Override
     public void bookAdded(BooksEvent booksEvent) {
+        log.info("Book added: {}", booksEvent);
         //return book to requesting function
         this.installedBook = booksEvent.getBook();
         lock.release();
