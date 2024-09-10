8a266ea98a75f3755a3a660dd53bb0bbd7e99fcc
Daniel <schoenke.dan@gmail.com>
2024-09-10 21:03:42 +0200

    filter chapter 0 or verse 0

---
+++ app/src/test/java/de/evangeliumstaucher/app/service/PassageTest.java
@@ -0,0 +1,29 @@
+package de.evangeliumstaucher.app.service;
+
+import de.evangeliumstaucher.repo.model.Bible;
+import de.evangeliumstaucher.repo.model.Passage;
+import de.evangeliumstaucher.sword.SwordLibrary;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PassageTest {
+
+    private static final String ID_SCHLACHTER = "sword-U2NobGFjaHRlciBCaWJlbCAoMTk1MSk=";
+    @Autowired
+    public SwordLibrary swordLibrary;
+
+    @Test
+    public void testOutputBibles() {
+        for (Bible bible : swordLibrary.getBibles()) {
+            System.out.println(bible.toString());
+        }
+    }
+
+    @Test
+    public void testKeys() {
+        Passage p = swordLibrary.getPassage(ID_SCHLACHTER, "1Mo1:0");
+        String c = p.getContent();
+    }
+}
