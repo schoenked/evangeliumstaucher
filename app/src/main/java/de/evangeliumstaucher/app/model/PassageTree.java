d0455746bc3f1a2663b7b15808143ae116d5b7da
Daniel <schoenke.dan@gmail.com>
2024-11-09 21:43:37 +0100

    getting passagetree

--- app/src/main/java/de/evangeliumstaucher/app/model/PassageTree.java
+++ app/src/main/java/de/evangeliumstaucher/app/model/PassageTree.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 public class PassageTree {
     private final List<PassageTree> passageTrees;
+    private final String id;
 
     public PassageTree(String id, List<PassageTree> passageTrees) {
         super();
-
+        this.id = id;
         this.passageTrees = passageTrees;
     }
+
     public List<PassageTree> getPassageTrees() {
         return passageTrees;
     }
