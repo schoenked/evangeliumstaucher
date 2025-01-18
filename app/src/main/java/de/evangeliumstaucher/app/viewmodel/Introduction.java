9802d5a9f689fa3346413bdb7712e6a722151d42
Daniel <schoenke.dan@gmail.com>
2025-01-18 19:54:19 +0100

    introduction

---
+++ app/src/main/java/de/evangeliumstaucher/app/viewmodel/Introduction.java
@@ -0,0 +1,23 @@
+package de.evangeliumstaucher.app.viewmodel;
+
+public class Introduction {
+    public static final String TEXT = """
+            ✝️🌍📖🤿
+            Wie funktioniert der evangeliumstaucher?
+            1️⃣ Dir werden nacheinander Bibelverse angezeigt
+            2️⃣ Lies den Bibelvers durch
+            3️⃣ Überlege wo der Vers stehen könnte
+                -   Lass dir auch die Verse davor und danach anzeigen
+                    Diese können dir helfen den Vers einzuordnen
+            4️⃣ Im unteren Bereich lässt sich die Bibelstelle auswählen
+            5️⃣ In der Auswertung siehst du das ERgebnis deiner Auswahl und der anderen Spieler
+            6️⃣ 2 Kriterien werden bewertet
+                📈🎯 Präzision - der Abstand deiner Auswahl zum tatsächlichen Vers
+                📈⌛ Dauer - die verstrichene Zeit bis du die Auswahl getroffen hast
+                📈📖 NICHT negativ bewertet werden die Verse, die du zusätzlich liest, denn
+            das ist das eigentliche Ziel: Dass Menschen die Bibel mehr lesen!
+            
+            Wenn du nicht alles verstanden hast, ist das bestimmt kein Problem.
+            Du lernst es sicher wenn du es ausprobierst. Bereit?
+            """;
+}
