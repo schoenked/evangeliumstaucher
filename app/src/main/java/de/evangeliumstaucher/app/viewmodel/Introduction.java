904bf3c0baa0c6312bfc1a01012e2f881d04ef13
Daniel <schoenke.dan@gmail.com>
2025-01-18 20:12:48 +0100

    introduction

--- app/src/main/java/de/evangeliumstaucher/app/viewmodel/Introduction.java
+++ app/src/main/java/de/evangeliumstaucher/app/viewmodel/Introduction.java
@@ -4,20 +4,20 @@ public class Introduction {
     public static final String TEXT = """
             ✝️🌍📖🤿
             Wie funktioniert der evangeliumstaucher?
-            1️⃣ Dir werden nacheinander Bibelverse angezeigt
-            2️⃣ Lies den Bibelvers durch
-            3️⃣ Überlege wo der Vers stehen könnte
-                -   Lass dir auch die Verse davor und danach anzeigen
+            1️⃣ 📜 Dir werden nacheinander Bibelverse angezeigt
+            2️⃣ 👓 Lies den Bibelvers durch
+            3️⃣ 💭 Überlege wo der Vers stehen könnte
+                -   📖 Lass dir auch die Verse davor und danach anzeigen
                     Diese können dir helfen den Vers einzuordnen
-            4️⃣ Im unteren Bereich lässt sich die Bibelstelle auswählen
-            5️⃣ In der Auswertung siehst du das ERgebnis deiner Auswahl und der anderen Spieler
-            6️⃣ 2 Kriterien werden bewertet
-                📈🎯 Präzision - der Abstand deiner Auswahl zum tatsächlichen Vers
-                📈⌛ Dauer - die verstrichene Zeit bis du die Auswahl getroffen hast
-                📈📖 NICHT negativ bewertet werden die Verse, die du zusätzlich liest, denn
-            das ist das eigentliche Ziel: Dass Menschen die Bibel mehr lesen!
+            4️⃣ ✅ Im unteren Bereich lässt sich die Bibelstelle auswählen
+            5️⃣ 📊 In der Auswertung siehst du das ERgebnis deiner Auswahl und der anderen Spieler
+            6️⃣ 🏆 2 Kriterien werden bewertet
+                    📈🎯 Präzision - der Abstand deiner Auswahl zum tatsächlichen Vers
+                    📈⌛ Dauer - die verstrichene Zeit bis du die Auswahl getroffen hast
+                    📈📖 NICHT negativ bewertet werden die Verse, die du zusätzlich liest, denn
+            das ist das eigentliche Ziel: Dass Menschen die BIBEL mehr lesen!
             
-            Wenn du nicht alles verstanden hast, ist das bestimmt kein Problem.
-            Du lernst es sicher wenn du es ausprobierst. Bereit?
+            🛟 Wenn du nicht alles verstanden hast, ist das bestimmt kein Problem.
+            Du lernst es sicher wenn du es ausprobierst.    Bereit?🤿
             """;
 }
