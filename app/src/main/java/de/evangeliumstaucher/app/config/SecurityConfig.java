13099da9c02168dbe92de87b998ff4d1359679b6
Daniel <schoenke.dan@gmail.com>
2026-04-18 19:22:52 +0200

    rejection filter only on proflie

--- app/src/main/java/de/evangeliumstaucher/app/config/SecurityConfig.java
+++ app/src/main/java/de/evangeliumstaucher/app/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.evangeliumstaucher.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
@@ -16,6 +17,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Profile("accounts")
     public EmailRejectionFilter emailRejectionFilter(AuthenticationManager authenticationManager, AccountConfig accountConfig) throws Exception {
         EmailRejectionFilter filter = new EmailRejectionFilter(accountConfig);
         filter.setAuthenticationManager(authenticationManager);
